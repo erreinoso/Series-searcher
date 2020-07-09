38f4c395a803c66f7033abdd22124ef2fd1cf56d
erreinoso <eramirezreinoso@gmail.com>
2020-07-09 23:48:26 +0200

    push to favourite array and paint it on the left side

--- main.js
+++ main.js
@@ -5,39 +5,96 @@ const input = document.querySelector('.js-input');
 const btn = document.querySelector('.js-button');
 const resultsectionp = document.querySelector('.js-resultSection-p');
 const resultsection = document.querySelector('.js-ul-result');
-console.log(input);
-
+const favsection = document.querySelector('.js-ul-fav');
+const imgTemporary = 'https://via.placeholder.com/210x295/ffffff/666666/';
 let seriesresult = [];
-let favoriteseries = [];
+let seriesalreadysearched = [];
+let favouriteseries = [];
 
+//👉 traigo los datos del API
 function getSeries() {
   //esta función trae datos de la API
   let searchinput = input.value;
-  console.log(searchinput);
   fetch(`http://api.tvmaze.com/singlesearch/shows?q=${searchinput}`)
     .then((response) => response.json())
-    .then((result) => {
-      seriesresult = result; //ya no es un array vacío
+    .then((data) => {
+      seriesresult = data; //ya no es un array vacío
       //primero consoleamos para ver si hemos encontrado el dato
-      renderSearch();
-      //ejecutamos la funcion que pinta dentro de la funcion que se ejecuta al hacer click
+      console.log('resultado de la busqueda', seriesresult);
+      renderSearch(seriesresult);
+      addListeners();
     });
 }
 
-function renderSearch() {
-  //función que pinta los datos en el HTML
+//👉 pinto los datos de la busqueda
 
-  let name = seriesresult.name;
-  let img = seriesresult.image.medium;
-  const imgTemporary = 'https://via.placeholder.com/210x295/ffffff/666666/';
-  if (img === null) {
-    //revisar esta parte
-    img = imgTemporary;
-  }
+function renderSearch(seriesresult) {
+  resultsectionp.classList.add('hidden');
+  seriesalreadysearched.push(seriesresult); // añado a la lista de buscados
+  console.log('series buscadas', seriesalreadysearched);
   let seriescard;
-  seriescard = `<li class="serie"><img src="${img}">`;
+  if (seriesresult.img === null) {
+    seriesresult.img = imgTemporary; //revisar esta parte
+  }
+  seriescard = `<li class="seriecard" id="${seriesresult.id}">`;
+  seriescard += `<img src="${seriesresult.image.medium}" alt="Foto de ${seriesresult.name}">`;
   seriescard += `<h3>${seriesresult.name}</h3></li>`;
   resultsection.innerHTML = seriescard;
+  console.log('texto que voy a insertar', seriescard);
 }
 
+// 👉  Creamos una funcion para que me escuchen las tarjetas al hacer click
+function addListeners() {
+  let liElem = document.querySelectorAll('.seriecard');
+  for (const li of liElem) {
+    li.addEventListener('click', favouritesHandler);
+  }
+}
+
+//👉 cuando hago clic sobre la tarjeta,
+// // -añadir a array favouriteseries         addToFavouritesArray
+// // -pintar dicho array en la sección     addToFavouritesSection
+// // - intercambiar color fuente y fondo          changeColor
+// // - guardar en local
+function favouritesHandler(ev) {
+  const clickedcard = ev.currentTarget;
+  console.log('elemento clicado', ev.currentTarget);
+  addToFavouritesArray(ev);
+  addToFavouritesSection(ev);
+  changeColor(ev);
+  saveIntoLocal();
+}
+
+function addToFavouritesArray(ev) {
+  const clickedcard = ev.currentTarget; //Esto funciona porque el resultado es solo 1
+  favouriteseries.push(seriesresult);
+  console.log('los favoritos son', favouriteseries);
+}
+
+function addToFavouritesSection() {
+  let seriesfav;
+  for (const serie of favouriteseries) {
+    seriesfav += `<li class="seriefavcard" id="${serie.id}"><img src="${serie.image.medium}" alt="Foto de ${serie.name}">`;
+    seriesfav += `<h3>${serie.name}</h3></li>`;
+    favsection.innerHTML = seriesfav;
+  }
+}
+
+function changeColor(ev) {
+  ev.currentTarget.classList.add('favelement');
+}
+
+// Listeners
 btn.addEventListener('click', getSeries);
+
+//Local storage
+function saveIntoLocal() {
+  localStorage.setItem('favouriteSeries', JSON.stringify(favouriteseries));
+}
+
+// function addToFavouritesArray(ev) {
+//   const clickedcard = ev.currentTarget;
+//   favouriteseries.push(seriesresult);
+//   console.log('elemento clickado', ev.currentTarget);
+//   console.log('el array de favoritos', favouriteseries);
+// } ESTO FUNCIONA PERO NO SERÍA LO MÁS LICITO?
