1e14215f3b0ec11da0c05d5bc9746dc16b6f192d
erreinoso <eramirezreinoso@gmail.com>
2020-07-10 09:14:21 +0200

    fixed api link and search

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ let favouriteseries = [];
 function getSeries() {
   //esta función trae datos de la API
   let searchinput = input.value;
-  fetch(`http://api.tvmaze.com/singlesearch/shows?q=${searchinput}`)
+  fetch(`http://api.tvmaze.com/search/shows?q=${searchinput}`)
     .then((response) => response.json())
     .then((data) => {
       seriesresult = data; //ya no es un array vacío
@@ -33,14 +33,16 @@ function renderSearch(seriesresult) {
   seriesalreadysearched.push(seriesresult); // añado a la lista de buscados
   console.log('series buscadas', seriesalreadysearched);
   let seriescard;
-  if (seriesresult.img === null) {
-    seriesresult.img = imgTemporary; //revisar esta parte
+  for (let i = 0; i <= seriesresult.length; i++) {
+    if (seriesresult.img === null) {
+      seriesresult.img = imgTemporary; //revisar esta parte
+    }
+    seriescard = `<li class="seriecard" id="${seriesresult[i].show.id}">`;
+    seriescard += `<img src="${seriesresult[i].show.image.medium}" alt="Foto de ${seriesresult[i].show.name}">`;
+    seriescard += `<h3>${seriesresult[i].show.name}</h3></li>`;
+    resultsection.innerHTML += seriescard;
+    console.log('texto que voy a insertar', seriescard);
   }
-  seriescard = `<li class="seriecard" id="${seriesresult.id}">`;
-  seriescard += `<img src="${seriesresult.image.medium}" alt="Foto de ${seriesresult.name}">`;
-  seriescard += `<h3>${seriesresult.name}</h3></li>`;
-  resultsection.innerHTML = seriescard;
-  console.log('texto que voy a insertar', seriescard);
 }
 
 // 👉  Creamos una funcion para que me escuchen las tarjetas al hacer click
@@ -56,6 +58,7 @@ function addListeners() {
 // // -pintar dicho array en la sección     addToFavouritesSection
 // // - intercambiar color fuente y fondo          changeColor
 // // - guardar en local
+
 function favouritesHandler(ev) {
   const clickedcard = ev.currentTarget;
   console.log('elemento clicado', ev.currentTarget);
@@ -65,23 +68,32 @@ function favouritesHandler(ev) {
   saveIntoLocal();
 }
 
-function addToFavouritesArray(ev) {
-  const clickedcard = ev.currentTarget; //Esto funciona porque el resultado es solo 1
-  favouriteseries.push(seriesresult);
-  console.log('los favoritos son', favouriteseries);
-}
+// function addToFavouritesArray(ev) {
+//   const clickedcard = ev.currentTarget.id; //Esto funciona porque el resultado es solo 1
+//   console.log('el id de laserie clickada', clickedcard);
+//   for (let favourite of favouriteseries) {
+//     if (favourite.id === parseInt(clickedcard)) {
+//       // favouriteseries.remove(seriesresult);
+//       console.log('ya está añadida a favoritos');
+//     } else {
+//       // favouriteseries.push(seriesresult);
+//       console.log('aun no  está añadida a favoritos');
+//     }
+//   }
+//   console.log('los favoritos son', favouriteseries);
+// }
 
-function addToFavouritesSection() {
-  let seriesfav;
-  for (const serie of favouriteseries) {
-    seriesfav += `<li class="seriefavcard" id="${serie.id}"><img src="${serie.image.medium}" alt="Foto de ${serie.name}">`;
-    seriesfav += `<h3>${serie.name}</h3></li>`;
-    favsection.innerHTML = seriesfav;
-  }
-}
+// function addToFavouritesSection() {
+//   let seriesfav;
+//   for (const serie of favouriteseries) {
+//     seriesfav += `<li class="seriefavcard" id="${serie.show.id}"><img src="${serie.show.image.medium}" alt="Foto de ${serie.show.name}">`;
+//     seriesfav += `<h3>${serie.show.name}</h3></li>`;
+//     favsection.innerHTML = seriesfav;
+//   }
+// }
 
 function changeColor(ev) {
-  ev.currentTarget.classList.add('favelement');
+  ev.currentTarget.classList.toggle('favelement');
 }
 
 // Listeners
@@ -98,3 +110,8 @@ function saveIntoLocal() {
 //   console.log('elemento clickado', ev.currentTarget);
 //   console.log('el array de favoritos', favouriteseries);
 // } ESTO FUNCIONA PERO NO SERÍA LO MÁS LICITO?
+
+// function addToFavouritesArray(ev) {
+//   const clickedcard = ev.currentTarget; //Esto funciona porque el resultado es solo 1
+//   favouriteseries.push(seriesresult);
+//   funcionaba
